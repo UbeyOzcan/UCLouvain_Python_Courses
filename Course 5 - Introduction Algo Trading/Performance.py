b6299d2ae7184f4f37d8d85f297a3d1c4843afc4
UbeyOzcan <ozcanubey@outlook.com>
2023-12-18 22:58:32 +0100

    Remove old docs

--- Course 5 - Introduction Algo Trading/Performance.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def Performance_calculator(buyprices, sellprices):
-    rtn = (pd.Series([(sell - buy) / buy for sell, buy in zip(sellprices, buyprices)]) + 1).prod() - 1
-    return round(rtn * 100, 2)
